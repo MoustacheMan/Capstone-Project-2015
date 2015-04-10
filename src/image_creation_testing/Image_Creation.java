14d6412f240b4153e27e23c3f6c31e79b38be654
David Gonzalez <moustachemaniac.95@gmail.com>
2015-04-09 21:49:53 -0300

    Made more head way into how to manipulate omages at the pixel in Java, and how to generate an image from scratch.

---
+++ src/image_creation_testing/Image_Creation.java
@@ -0,0 +1,11 @@
+package image_creation_testing;
+
+import java.io.File;
+
+public class Image_Creation {
+
+	public static void main(String[] args) {
+		File imageFile = new File("Test-Images/test1.jpg");
+		
+	}
+}
--- src/image_creation_testing/Image_Modification.java
+++ src/image_creation_testing/Image_Modification.java
@@ -4,6 +4,9 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 
@@ -17,6 +20,40 @@ public class Image_Modification {
 		
 		try {
 			BufferedImage image = ImageIO.read(imageFile);
+			int midWidth = image.getWidth() / 2;
+			int midHeight = image.getHeight();
+			
+			
+			WritableRaster wra = image.copyData(null);
+			int numPixles = wra.getPixels(midWidth, midHeight, 10, 10, (int[])null).length;
+			int[] newPix = new int[numPixles];
+			for (int i = 0; i < newPix.length; i++) {
+				newPix[i] = 0;
+				
+			}
+			wra.setPixels(midWidth, midHeight, 10, 10, newPix);
+			System.out.println("Copied data dimensions " + wra.getWidth() + "x" + wra.getHeight());
+			
+			ColorModel cModel = image.getColorModel();
+			System.out.println("Pixel Size: " + cModel.getPixelSize());
+			Raster ra = image.getData();
+			int[] middlePixels = ra.getPixels(image.getWidth() / 2, image.getHeight() / 2, 10, 10, (int[])null);
+			int [] middlePixel = ra.getPixel(image.getWidth() / 2, image.getHeight() / 2, (int[])null);
+			
+			for (int i : middlePixel) {
+				System.out.println(i);
+			}
+			System.out.println("Printing rectangle of pixels");
+//			System.out.println(middlePixels[0]);
+			int count = 0;
+//			for (int i : middlePixels) {
+//				System.out.println(i);
+//				count++;
+//			}
+			System.out.println("Count: " + count);
+			
+			
+			
 			Graphics2D gr = image.createGraphics();
 			
 			gr.setColor(Color.BLACK);
